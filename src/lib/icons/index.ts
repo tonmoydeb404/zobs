1f3de27623c3411e6c98a3fff2fbe33e6548fedb
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-08-06 13:27:47 +0600

    feat: installed rnr setup

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,7 @@
+import { MoonStar, Sun } from 'lucide-react-native';
+import { iconWithClassName } from './with-classname';
+
+iconWithClassName(Sun);
+iconWithClassName(MoonStar);
+
+export { MoonStar, Sun };
